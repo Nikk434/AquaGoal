b1ac8fbb2048e7841c8a8da58f7b81457efefc4c
Nikk434 <nskshirsagar434@gmail.com>
2024-11-24 12:23:26 +0530

    added navigation

--- aqua-goal/src/Component/HomeComponent.jsx
+++ aqua-goal/src/Component/HomeComponent.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+// import TrackDaily from './TrackDaily'
 export default function HomePage() {
+    const navigate = useNavigate(); 
     return(
         <div>
             <h1>hello</h1>
+            <table>
+                <td>
+                    <button onClick={()=> navigate('/TrackDaily') }>track daily</button>
+                    
+                </td>
+                <td><button onClick={()=> navigate('/CreateChallenge')}>create challenge</button></td>
+                
+            </table>
         </div>
     )
 }
