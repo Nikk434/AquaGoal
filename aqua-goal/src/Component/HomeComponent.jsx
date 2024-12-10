fa6b72b87a7cc1e3ace931beb514b68a65cef129
Nikk434 <nskshirsagar434@gmail.com>
2024-12-10 11:06:05 +0530

    Minor changes

--- aqua-goal/src/Component/HomeComponent.jsx
+++ aqua-goal/src/Component/HomeComponent.jsx
@@ -1,18 +1,41 @@
 import { useNavigate } from 'react-router-dom';
-// import TrackDaily from './TrackDaily'
+
 export default function HomePage() {
     const navigate = useNavigate(); 
+
     return(
-        <div>
-            <h1>hello</h1>
-            <table>
-                <td>
-                    <button onClick={()=> navigate('/TrackDaily') }>track daily</button>
-                    
-                </td>
-                <td><button onClick={()=> navigate('/CreateChallenge')}>create challenge</button></td>
-                
+        <div className="container text-center">
+            <h1 className="my-4">Hello</h1>
+            <table className="table">
+                <tbody>
+                    <tr>
+                        <td className="text-center">
+                            <button 
+                                className="btn btn-primary" 
+                                onClick={() => navigate('/TrackDaily')}
+                            >
+                                Track Daily
+                            </button>
+                        </td>
+                        <td className="text-center">
+                            <button 
+                                className="btn btn-success" 
+                                onClick={() => navigate('/CreateChallenge')}
+                            >
+                                Create Challenge
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="text-center">
+                            Set a daily goal to achieve
+                        </td>
+                        <td className="text-center">
+                            Compete with friends to be No. 1
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
-    )
+    );
 }
